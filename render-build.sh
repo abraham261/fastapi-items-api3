1e1eb220b2f78fe88251153fffdf20bb6622f8e4
abraham261 <abrahamleonard331@gmail.com>
2025-04-29 17:50:32 +0200

    Fix permissions pour render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+echo '=== Vérification des fichiers ==='
+ls -la
+echo '=== Emplacement requirements.txt ==='
+find . -name requirements.txt
+echo '=== Installation dépendances ==='
+pip install -r requirements.txt
